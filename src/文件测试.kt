52ed31de12d0f85e5792dcf335bebb3a2effe341
lanut <997622499@qq.com>
2024-05-27 20:17:35 +0800

    多线程文件测试添加四元式生成

--- src/文件测试.kt
+++ src/文件测试.kt
@@ -1,9 +1,11 @@
 import com.alibaba.fastjson2.toJSONString
+import homework.grammatical.entity.SyntaxTree
 import homework.grammatical.utils.outputSVGFile
 import homework.grammatical.utils.toUmlTree
 import homework.grammatical.语法分析器
 import homework.lexical.codeStringToTokenList
 import homework.lexical.entity.Category
+import homework.quaternionTranslators.QuaternionGeneration
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
@@ -87,18 +89,19 @@ fun singleThreadedOutput(path: Path) {
     }
 }
 
-
+// 多线程输出：词法分析 -> 语法分析 -> 四元式生成
 @OptIn(DelicateCoroutinesApi::class)
 fun multithreadedOutput(path: Path) {
-    // 创建一个Channel，用于在生产者和消费者之间传递File对象
+    // 创建一个Channel，用于词法分析器和语法分析器之间的通信
     val fileChannel = Channel<File>()
+    // 创建一个Channel，用于语法分析器与四元式生成器之间的通信
+    val syntaxTreeChannel = Channel<SyntaxTree>()
     val directory = File(path.toUri())
     if (!directory.isDirectory) throw Exception("此不是合法的文件夹")
     val errorList = mutableListOf<String>()
 
-    // 生产者协程
-    val producer = GlobalScope.launch {
 
+    val 词法分析 = GlobalScope.launch {
         val sampleFiles = directory.listFiles()
         sampleFiles?.filter {
             it.name.endsWith(".sample")
@@ -121,13 +124,12 @@ fun multithreadedOutput(path: Path) {
             }.toJSONString()
             val outputFile = File("$path/${file.nameWithoutExtension}.js")
             outputFile.writeText(jsStr)
-            fileChannel.send(outputFile)
+            fileChannel.send(outputFile) // 发送文件
         }
-        fileChannel.close()
+        fileChannel.close() // 关闭文件Channel
     }
 
-    // 消费者协程
-    val consumer = GlobalScope.launch {
+    val 语法分析 = GlobalScope.launch {
         for (file in fileChannel) {
             val content = File(file.path).readText()
             val expressionStore = try {
@@ -142,18 +144,53 @@ fun multithreadedOutput(path: Path) {
                 File("$path/${file.nameWithoutExtension}_Error.txt").writeText(sb.toString())
                 continue
             }
-            val syntaxTree = expressionStore.toTree().apply { tidy() }
+            val syntaxTree = expressionStore.toTree(name = file.nameWithoutExtension).apply { tidy() }
+            syntaxTreeChannel.send(syntaxTree) // 发送语法树
             val umlTree = syntaxTree.toUmlTree("${file.nameWithoutExtension} 语法树")
             File("$path/${file.nameWithoutExtension}_Expression.txt").writeText(expressionStore.toString())
             outputSVGFile(umlTree, "$path/${file.nameWithoutExtension}.svg")
         }
+        syntaxTreeChannel.close() // 关闭语法树Channel
+    }
+
+    val 四元式生成 = GlobalScope.launch {
+        for (syntaxTree in syntaxTreeChannel) {
+            val quaternionGeneration = QuaternionGeneration()
+            try {
+                quaternionGeneration.程序(syntaxTree.root)
+            } catch (e: Exception) {
+                val errorName = "四元式生成错误文件名: ${syntaxTree.name}.txt"
+                errorList.add(errorName)
+                val sb = StringBuffer()
+                sb.appendLine(errorName)
+                sb.appendLine(e.stackTraceToString())
+                sb.appendLine()
+                File("$path/${syntaxTree.name}_Error_quaternionGeneration.txt").writeText(sb.toString())
+                continue
+            }
+            val outputFile = File("$path/${syntaxTree.name}_Quaternion.txt")
+            val sb = StringBuffer()
+            sb.append("main 函数四元式：\n")
+            sb.append(quaternionGeneration.qExpressionList.joinToString("\n"))
+            for (function in quaternionGeneration.functionList) {
+                sb.append("\n\n\n${function.name} 函数参数列表：\n")
+                sb.append(function.parameterList.joinToString("\t"))
+                sb.append("${function.name} 函数四元式：\n")
+                sb.append(function.qExpressionList.joinToString("\n"))
+            }
+            outputFile.writeText(sb.toString())
+        }
     }
 
 
-    // 等待生产者和消费者协程完成
     runBlocking {
-        producer.join()
-        consumer.join()
+        词法分析.join()
+        println("词法分析完成")
+        语法分析.join()
+        println("语法分析完成")
+        四元式生成.join()
+        println("四元式生成完成")
+        println()
         if (errorList.isNotEmpty()) {
             val sb = StringBuffer()
             sb.append("总计出现${errorList.size}个错误：\n")
